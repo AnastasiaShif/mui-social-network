0f859b848f78ef24d2c5ed8038f077b170d44404
Anton Shifman <santonsh@gmail.com>
2023-06-20 09:35:40 +0300

    my first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import { Sidebar } from "./components/Sidebar";
+import { Navbar } from "./components/Navbar";
+import { Rightbar } from "./components/Rightbar";
+import { Feed } from "./components/Feed";
+import React from "react";
+import { Box, Stack } from "@mui/material";
+import Add from "./components/Add";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Box>
+      <Navbar />
+      <Stack direction="row" justifyContent="space-between" spacing={2}>
+        <Sidebar />
+        <Feed />
+        <Rightbar />
+      </Stack>
+      <Add />
+    </Box>
   );
 }
 
